faf43072305051c2668775bbaaebe8bbd85b1033
Karan Jadaun <jadaunfamily6@gmail.com>
2021-06-10 18:24:30 +0530

    Update dynamic-calculator.py

--- dynamic-calculator.py
+++ dynamic-calculator.py
@@ -1,5 +1,6 @@
 #Greetings
 print("Hello!, Select Operation you want to do: ")
+
 #Operation to be performed.
 print("1. Addition")
 print("2. Subtraction")
@@ -23,14 +24,15 @@ while True:
                 result= num1 + num2
                 print("The Sum is: ",result)
                 print()
+                
         elif(i==2):
                 print("Input Two Numbers to Subtract : ")
                 num1 = float(input("Enter Number 1: "))
                 num2 = float(input("Enter Number 2: "))
-
                 result = num1 - num2
                 print("The Difference is: ", result)
                 print()
+                
         elif(i==3):
                 print("Input Two Numbers to Multiply : ")
                 num1 = float(input("Enter Number 1: "))
@@ -38,6 +40,7 @@ while True:
                 result = num1 * num2
                 print("The Product is: ", result)
                 print()
+                
         elif (i == 4):
                 print("Input Two Numbers to Divide : ")
                 num1 = float(input("Enter Number 1: "))
@@ -45,8 +48,11 @@ while True:
                 result = num1 / num2
                 print("The Division is: ", result)
                 print()
+                
         break
 
     else:
        print("Invalid Input !! Please Try Again")
        print()
+
+# Thank you
